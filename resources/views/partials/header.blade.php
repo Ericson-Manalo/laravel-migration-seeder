84b4666ef5295e758bdf233eb3b9a2245da72821
Ericson-Manalo <ericmana98@gmail.com>
2023-07-21 16:52:22 +0200

    took a header by bootstrap

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand ms-3" href="#"><i class="fa-solid fa-train fs-4"></i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link fs-4" href="{{ route ('home') }}">Home</a>
+                <a class="nav-item nav-link fs-4" href="{{route('train')}}">Trains</a>
+            </div>
+        </div>
+    </nav>
+</header>
