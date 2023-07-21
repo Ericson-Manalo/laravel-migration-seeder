538f74169c9b42513c22027f102a02e63063836b
Ericson-Manalo <ericmana98@gmail.com>
2023-07-21 18:58:53 +0200

    created array by using faker function

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -14,8 +14,9 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+
         // $trainList = [
         //     [
         //         'agency' => 'Frecciarossa',
@@ -41,20 +42,80 @@ class TrainSeeder extends Seeder
         //     ]
         // ];
 
-        foreach ($trainList as $train){
+
+        $agencyList = [
+            "EuroCity",
+            "Frecciarossa Alta Velocità",
+            "Italo",
+            "InterCity",
+            "Suburbano",
+            "Regionale",
+            "Regionale Veloce",
+            "RegioExpress",
+            "Frecciabianca",
+            "Frecciargento",
+            "Euronight"
+        ];
+
+        $stationList = [
+            "Ardenno Masino",
+            "Castel D`Ario",
+            "Certosa Di Pavia",
+            "Broni",
+            "Chiari",
+            "Chignolo Po",
+            "Chiuduno",
+            "Cisano Caprino Bergamasco",
+            "Grumello Del Monte",
+            "Lecco Maggianico",
+            "Bellano Tartavelle Terme",
+            "Milano C.le",
+            "Carbonara Scrivia",
+            "Tonco Alfiano",
+            "Torino Smistamento Nord",
+            "Mussotto",
+            "Villafranca C.",
+            "Ponzana",
+            "Magliano Crava Morozzo",
+            "S. Germano Vercellese",
+            "Vergnasco",
+            "Vigliano D`Asti",
+            "Madonna Del Pilone",
+            "Margarita",
+            "Nichelino",
+        ];
+
+        $boolTrain = [
+            true,
+            false,
+        ];
+
+        // foreach ($trainList as $train){
+        //     $newTrain = new Train();
+        //     $newTrain->agency = $train['agency'];
+        //     $newTrain->departure_station = $train['departure_station'];
+        //     $newTrain->arrival_station = $train['arrival_station'];
+        //     $newTrain->departure_time = $train['departure_time'];
+        //     $newTrain->arrival_time = $train['arrival_time'];
+        //     $newTrain->train_code = $train['train_code'];
+        //     $newTrain->carriages_num = $train['carriages_num'];
+        //     $newTrain->in_time = $train['in_time'];
+        //     $newTrain->canceled = $train['canceled'];
+        //     $newTrain->save();
+        //}
+
+        for ($i=0; $i < 100 ; $i++) { 
             $newTrain = new Train();
-            $newTrain->agency = $train['agency'];
-            $newTrain->departure_station = $train['departure_station'];
-            $newTrain->arrival_station = $train['arrival_station'];
-            $newTrain->departure_time = $train['departure_time'];
-            $newTrain->arrival_time = $train['arrival_time'];
-            $newTrain->train_code = $train['train_code'];
-            $newTrain->carriages_num = $train['carriages_num'];
-            $newTrain->in_time = $train['in_time'];
-            $newTrain->canceled = $train['canceled'];
-            $newTrain->save();
-
-
+                $newTrain->agency = $faker->randomElement($agencyList);
+                $newTrain->departure_station = $faker->randomElement($stationList);
+                $newTrain->arrival_station = $faker->randomElement($stationList);
+                $newTrain->departure_time = $faker->time('H:i:s');
+                $newTrain->arrival_time = $faker->time('H:i:s');
+                $newTrain->train_code = $faker->bothify('CAXG-1742!');
+                $newTrain->carriages_num = $faker->randomDigit();
+                $newTrain->in_time = $faker->randomElement($boolTrain);
+                $newTrain->canceled = $faker->randomElement($boolTrain);
+                $newTrain->save();
         }
         
     }
